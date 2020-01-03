51b18843b5fd0794e9ba955789417ef2303fb1ad
Robert Englund <kastanjen3@gmail.com>
2020-01-03 20:24:55 +0100

    a

--- fly/script.js
+++ fly/script.js
@@ -5,44 +5,156 @@ setInterval(function(){
 	secondsLeft--;
 
 	if ( secondsLeft == 0 ) {
-		
+		stop();
 	}
 
 }, 1000);
 setInterval(function(){ 
 
-	var curr = parseInt($("#pointer").css("left"));
-	var width = parseInt($("#path").width());
-	var pos = (curr / width) * 100 + (forceRight - forceLeft);
+	if ( shouldMove ) {
+		var curr = parseInt($("#pointer").css("left"));
+		var width = parseInt($("#path").width());
+		var pos = (curr / width) * 100 + (forceRight - forceLeft) + randForce;
 
-	console.log(curr);
-	console.log(width);
-	console.log(pos);
+		if ( pos >= 100 ) {
+			pos = 99.9;
+		} 
+		else if ( pos <= 0 ) {
+			pos = 0.1;
+		}
 
-	$("#pointer").css( "left", pos + "%" );
+		$("#pointer").css( "left", pos + "%" );
+	}
 
 }, 100);
+setInterval(function(){ 
+
+	var rand = Math.random() * 2 - 1;
+	while ( rand == 0 ) {
+		rand = Math.random() * 2 - 1;
+	}
+	randForce = rand;
+
+}, 10000);
+
+var shouldMove = false;
+var playing = false;
 
 var attempts = [];
-var lastNumber;
-var currentNumber;
+var lastNumber = 0;
+var currentNumber = 0;
 
 var forceLeft = 0;
 var forceRight = 0;
+var randForce = 0.5;
 
-function startTimer() {
+function start() {
 
-	secondsLeft = 3 * 60;
+	secondsLeft = 0.5 * 60;
 
 	$("#start").css("display", "none");
 	$("#main-container").css("display", "block");
 	$("#score-container").css("display", "none");
 
+	$("#pointer").css( "left", "50%" );
+
 	attempts = [];
+	shouldMove = true;
+	playing = true;
+
+	generateNumber();
+
+}
+
+function stop() {
+
+	$("#score-container").empty();
+
+	shouldMove = false;
+	playing = false;
+
+	$("#start").css("display", "block");
+	$("#main-container").css("display", "none");
+	$("#score-container").css("display", "block");
+
+	var correct = 0;
+		var wrong = 0;
+		for ( var i = 0; i < attempts.length; i++ ) {
+			if ( attempts[i].correct ) {
+				correct++;
+			}
+			else {
+				wrong++;
+			}
+		}
+
+		$("#score-container").append("<div>Rätt: "+correct+" Fel: "+wrong+"</div>");
+
+		for ( var i = 0; i < attempts.length; i++ ) {
+			if (attempts[i].correct) {
+				$("#score-container").append("<div class='correct'>Gissade: "+attempts[i].guess+" Var: "+attempts[i].was+"</div>");
+			}
+			else {
+				$("#score-container").append("<div class='wrong'>Gissade: "+attempts[i].guess+" Var: "+attempts[i].was+"</div>");
+			}
+		}
+
+}
+
+
+function guess( num ) {
+	var correct;
+
+	if ( num == lastNumber ) {
+		correct = true;
+	}
+	else {
+		correct = false;
+	}
+
+	var obj = {
+		correct: correct,
+		guess: num,
+		was: lastNumber
+	}
+
+	attempts.push(obj);
+	$("#container-numbers button").attr("disabled", true);
 
 
 }
 
+
+function generateNumber() {
+	var num = Math.floor(Math.random() * 3) + 1;
+
+	while ( num == lastNumber ) {
+		num = Math.floor(Math.random() * 3) + 1;
+	}
+
+	$("#target-number span").text(num);
+
+	if ( num != 0 ) {
+		$("#container-numbers button").attr("disabled", false);
+	}
+
+	setTimeout(function(){
+
+		lastNumber = num;
+		$("#container-numbers button").attr("disabled", true);
+		$("#target-number span").text("-");
+
+	},2000);
+
+	setTimeout(function(){
+
+		if ( playing ) {
+			generateNumber();
+		}
+
+	},5000);
+
+}
 
 
 $(document).keydown(function(e){
